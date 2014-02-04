f0f9393c6b7f7f1becf4438009aef833624a1d72
ahaurw01 <aaron.haurwitz@gmail.com>
2014-02-04 15:11:47 -0500

    Convert backslash to forward slash in template names

--- test/main.js
+++ test/main.js
@@ -17,6 +17,27 @@ var getFixture = function (filePath) {
   });
 };
 
+var getFixtureWindowsStyle = function (filePath) {
+  filePath = path.join('test', 'fixtures', filePath);
+  var file = new gutil.File({
+    path: filePath,
+    cwd: path.join('test', 'fixtures'),
+    base: path.join('test'),
+    contents: fs.readFileSync(filePath)
+  });
+  Object.defineProperty(file, 'relative', {
+    get: function () {
+      if (!this.base) throw new Error('No base specified! Can not get relative.');
+      if (!this.path) throw new Error('No path specified! Can not get relative.');
+      return path.relative(this.base, this.path).replace(/\//g, '\\');
+    },
+    set: function () {
+      throw new Error('You cannot do that!');
+    }
+  });
+  return file;
+};
+
 var getExpectedString = function (filePath) {
   return fs.readFileSync(path.join('test', 'expected', filePath), 'utf8');
 };
@@ -98,6 +119,24 @@ describe('gulp-ember-handlebars', function () {
         should.exist(newFile);
         should.exist(newFile.path);
         newFile.path.should.equal('test/fixtures/x.js');
+        done();
+      });
+      stream.write(basicTemplate);
+      stream.end();
+    });
+
+    it('should convert windows path separators to unix path separators', function (done) {
+      var stream = plugin({
+        outputType: 'browser',
+        namespace: 'NAMESPACE'
+      });
+
+      var basicTemplate = getFixtureWindowsStyle('nested/Basic.hbs');
+
+      stream.on('data', function (newFile) {
+        should.exist(newFile);
+        should.exist(newFile.contents);
+        fileMatchesExpected(newFile, 'Basic_nested.js');
         done();
       });
       stream.write(basicTemplate);
