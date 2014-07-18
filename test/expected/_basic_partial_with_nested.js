882a3fe2b800a80e9a70b4246872e49532363154
ebaranov <ebaranov.dev@gmail.com>
2014-07-18 21:48:20 +0300

    Fix unit tests

--- test/expected/_basic_partial_with_nested.js
+++ test/expected/_basic_partial_with_nested.js
@@ -1,4 +1,4 @@
-define(['handlebars','./_basic_nested_partial'], function(Handlebars,_basic_nested_partial) { return Handlebars.registerPartial("_basic_partial_with_nested", Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+define(['handlebars','./_basic_nested_partial'], function(Handlebars,_basic_nested_partial) { return (function() { var __cTemplate = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Handlebars.helpers); partials = this.merge(partials, Handlebars.partials); data = data || {};
   var buffer = "", stack1, self=this;
@@ -8,4 +8,4 @@ helpers = this.merge(helpers, Handlebars.helpers); partials = this.merge(partial
   stack1 = self.invokePartial(partials._basic_nested_partial, '_basic_nested_partial', depth0, helpers, partials, data);
   if(stack1 || stack1 === 0) { buffer += stack1; }
   return buffer;
-  })); });
+  }); Handlebars.registerPartial("_basic_partial_with_nested", __cTemplate); return __cTemplate; })(); });
