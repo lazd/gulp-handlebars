07d92bc52b0fb4eb21aa6c92244c1784ca6d5989
Larry Davis <lazdnet@gmail.com>
2014-08-14 10:56:19 -0700

    Update dependencies, fix tests to work with new gulp-define-module

--- test/expected/Basic_node.js
+++ test/expected/Basic_node.js
@@ -1,4 +1,4 @@
-var Handlebars = global.Handlebars || require('handlebars');module.exports = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+var Handlebars = require("handlebars");module.exports = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
   
