ae594dfad232a7c0bc5bd7739b0b01d07055dc3a
Larry Davis <lazdnet@gmail.com>
2015-03-03 10:53:56 -0800

    Added handlebars3-partials example, fix bad link in partials example

---
+++ examples/handlebars3-partials/gulpfile.js
@@ -0,0 +1,52 @@
+var path = require('path');
+var gulp = require('gulp');
+var wrap = require('gulp-wrap');
+var declare = require('gulp-declare');
+var concat = require('gulp-concat');
+var merge = require('merge-stream');
+/** REMOVE ME **/ var handlebars = require('../../');
+/** USE ME **/ // var handlebars = require('gulp-handlebars');
+
+gulp.task('templates', function() {
+  // Assume all partials start with an underscore
+  // You could also put them in a folder such as source/templates/partials/*.hbs
+  var partials = gulp.src(['source/templates/_*.hbs'])
+    .pipe(handlebars({
+      handlebars: require('handlebars')
+    }))
+    .pipe(wrap('Handlebars.registerPartial(<%= processPartialName(file.relative) %>, Handlebars.template(<%= contents %>));', {}, {
+      imports: {
+        processPartialName: function(fileName) {
+          // Strip the extension and the underscore
+          // Escape the output with JSON.stringify
+          return JSON.stringify(path.basename(fileName, '.js').substr(1));
+        }
+      }
+    }));
+
+  var templates = gulp.src('source/templates/**/[^_]*.hbs')
+    .pipe(handlebars({
+      handlebars: require('handlebars')
+    }))
+    .pipe(wrap('Handlebars.template(<%= contents %>)'))
+    .pipe(declare({
+      namespace: 'MyApp.templates',
+      noRedeclare: true // Avoid duplicate declarations
+    }));
+
+  // Output both the partials and the templates as build/js/templates.js
+  return merge(partials, templates)
+    .pipe(concat('templates.js'))
+    .pipe(gulp.dest('build/js/'));
+});
+
+gulp.task('copy', function(){
+  gulp.src('node_modules/handlebars/dist/handlebars.runtime.js')
+    .pipe(gulp.dest('build/js/'));
+
+  gulp.src('source/index.html')
+    .pipe(gulp.dest('build/'));
+});
+
+// Default task
+gulp.task('default', ['copy', 'templates']);
