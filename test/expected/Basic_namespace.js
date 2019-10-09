893d1c111ab0b42c3e345d27ecd4a254a4d9aa22
John Griffiths <johng@nulogy.com>
2019-10-09 18:12:43 -0400

    Update handlebars.

--- test/expected/Basic_namespace.js
+++ test/expected/Basic_namespace.js
@@ -1,3 +1,3 @@
-MyApp.templates["Basic"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+MyApp.templates["Basic"] = Handlebars.template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
     return "Basic template";
 },"useData":true});
--- test/expected/Basic_node.js
+++ test/expected/Basic_node.js
@@ -1,3 +1,3 @@
-var Handlebars = require("handlebars");module.exports = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+var Handlebars = require("handlebars");module.exports = Handlebars.template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
     return "Basic template";
 },"useData":true});
